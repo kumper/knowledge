5f9ae83759df565cccdf01a3f8d2edd47180c9b1
kumper <kumper@kumper-PC>
2014-10-14 22:36:34 +0200

    Spring Security Web Service

---
+++ SpringSecurityWebService/src/main/java/com/kp/ws/SpringSecurity/Application.java
@@ -0,0 +1,21 @@
+package com.kp.ws.SpringSecurity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        
+        log.debug("Application has started");
+    }
+}
